cbaf48765e96c725735d4b69897228f22b7b2039
chienfx <chienpham304@gmail.com>
2018-04-08 23:27:06 +0700

    check syntax

--- Time.asm
+++ Time.asm
@@ -3,43 +3,36 @@
 	userInfo2: .asciiz "\nYou typed in: "
 	userInput: .space 10
 	lengthInput: .space 4
+
+
+	#chien
+	strData: .space 11
+	newLine: .asciiz "\n"
+	slash:	.byte	'/'
 	
+	msg_Invaild: .asciiz "The date inputed is invaild"
+	msg_Vaild: .asciiz "The date inputed is Vaild"
+
 #-------------------------------------------------------------
 
 .text 
+main:
 	la $a0, userInfo1
 	li $v0, 4 		#in ra man hinh userInfo1
 	syscall	
 	
-	la $a0, userInput	#dia chi cua userInput gán vào $a0
-	la $a1, lengthInput	#?? dài c?a chu?i nh?p vô
-	li $v0, 8 		#nhap ngay thang nam tu nguoi dung
-	syscall
+	jal 	inputDate
+	add	$s0, $v0, $0	#$s0 = stringDate
 	
-	#move $s0, $a0
-	jal Year		#Tra ve tai $v0
-	move $a0, $v0		
-	
-	li $v0, 1
+	addi	$v0, $0, 4	
+	add	$a0, $s0, $0
 	syscall
-	#In ra man hinh user nhap
-	#la $a0, userInfo2
-	#li $v0, 4
-	#syscall 
-
-	#la $a0, userInput
-	#li $v0, 4
-	#syscall
 	
 	
 	#Thoát chuong trình
 	li $v0, 10
 	syscall
 	
-#-------------------------------------------------------------
-
-.globl main
-main: 
 
 #-------------------------------------------------------------
 
@@ -143,8 +136,97 @@ Year:
 	addu $sp, $sp, 4
 	
 	jr $ra
-<<<<<<< HEAD
-=======
 
->>>>>>> 36ee5f985b79a54302a420b1925b004a759c0d7f
 #-------------------------------------------------------------
+
+##################################################################################################
+inputDate: #input string to $s0
+	#read in string
+	addi	$v0, $0, 8
+	la	$a0, strData
+	addi	$a1, $0, 11
+	syscall
+	
+	addi	$sp, $sp, -4
+	sw	$ra, 0($sp)	
+
+	jal	checkSyntax
+	add	$t0, $v0, $0	#if $t0 = 0 -> syntax error
+	beq	$t0, $0, invaild
+
+	#checklogic
+	#jal	checkLogic
+	#add	$t0, $v0, $0	#if $t0 = 0 -> syntax error
+	#beq	$t0, $0, invaild
+
+
+	#vaild:
+	add	$v0, $a0, $0	#return $s0
+	lw	$ra, 0($sp)
+	jr	$ra
+
+	invaild:
+		addi	$v0, $0, 4
+		la	$a0, msg_Invaild
+		syscall
+		j 	exit	
+
+	exit:
+	addi 	$v0, $0, 10
+	syscall	
+
+#################################################
+checkSyntax:	#bool check(string str)
+
+	addi	$t4, $0, 47	# slash '/'
+	addi	$t5, $0, 57	# character '9'
+
+	add	$t0, $0, $0 	#	i=0
+
+        loop_st:
+	slti	$t1, $t0, 10	#	if i < 10 -> t1 = 1
+	beq	$t1, $0, out_st	#	if i >= 10 -> break
+	add	$t1, $a0, $t0	#	t1 = a0 + t0 <=> t1 = str+i
+	lb	$t2, 0($t1)	#	t2 = str[i]
+	
+	addi	$t1, $t2, -10	#	t1 = (str[i] == '\n')
+	beq	$t1, $0, out_st
+	
+
+	#check here
+	slt	$t1, $t2, $t4	#	t1 = (str[i] < 47)
+	bne	$t1, $0, end_st
+	slt	$t1, $t5, $t2	#	t1 = (57 < str[i])
+	bne	$t1, $0, end_st	#	if t1==1 -> return false
+	
+	addi	$t0, $t0, 1	# 	i++
+	j	loop_st
+
+	out_st:	#return true
+		addi	$v0, $0, 1
+		jr	$ra
+	end_st:	#return false
+		addi 	$v0, $0, 0
+		jr	$ra
+
+#################################################	
+checkLogic:	#bool checkLogic(string str)
+	
+		
+
+	out_lg:	#return true
+		addi	$v0, $0, 1
+		jr	$ra
+	end_lg:	#return false
+		addi 	$v0, $0, 0
+		jr	$ra
+
+
+#################################################
+printNewLine:
+	#print newline
+	addi	$v0, $0, 4
+	la	$a0, newLine
+	syscall
+
+	jr	$ra
